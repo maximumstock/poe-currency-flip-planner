ab2b91cd5190d75f25b282038ef4e078db594e32
Maximilian Stock <me@maximumstock.net>
2018-01-27 16:47:20 +0100

    Print necessary transactions in feasibility check

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from src.flip import *
 leagues = ["Abyss"]
 trading_currencies = list(currencies.keys())
 currency_combinations = list(itertools.permutations(trading_currencies, 2))
+print("Fetching offers for", trading_currencies)
 offers = [fetch_conversion_offers(leagues[0], c1, c2) for (c1, c2) in currency_combinations]
 
 graph = build_graph(offers)
@@ -12,6 +13,6 @@ for c in trading_currencies:
   print("Checking {}".format(c))
   paths = find_paths(graph, c, c)
   for p in paths:
-    rate = maximum_conversion_rate(p)
-    if rate > 1.0:
-      print("{} league: {} is profitable: {}".format(leagues[0], c, rate))
+    cp = calculate_path(p)
+    if cp['winnings'] > 0:
+      print(cp['transactions'])
