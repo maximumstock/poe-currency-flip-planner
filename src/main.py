cbde666f860cad58ae8a07832e6541ea39ae22da
Maximilian Stock <me@maximumstock.net>
2018-03-11 17:58:33 +0100

    fix main setup and sort by winnings

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from src.flip import *
 
 
 leagues = ["Bestiary"]
-trading_currencies = list(currencies.keys())
+trading_currencies = list(currencies.keys())[0:5]
 currency_combinations = list(itertools.permutations(trading_currencies, 2))
 print("Fetching offers for", trading_currencies)
 offers = [fetch_conversion_offers(leagues[0], c1, c2) for (c1, c2) in currency_combinations]
@@ -13,9 +13,10 @@ for c in trading_currencies:
   paths = find_paths(graph, c, c, 3)
   profitable_paths = []
   for p in paths:
-    cp = calculate_path(p)
+    cp = build_conversion(p)
     if cp is not None and cp['winnings'] > 0:
       profitable_paths.append(cp)
   print("Checking {} - {}".format(c, len(profitable_paths)))
+  profitable_paths = sorted(profitable_paths, key=lambda k: k['winnings'], reverse=True)
   for p in profitable_paths:
     print(p['winnings'], p['transactions'])
