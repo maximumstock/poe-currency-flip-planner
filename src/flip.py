bba838bac03263231622d9adadd9b9bea8efc0c3
Maximilian Stock <me@maximumstock.net>
2018-03-11 18:02:26 +0100

    Add some currency hierarchy

--- src/flip.py
+++ src/flip.py
@@ -3,46 +3,110 @@ import itertools
 from bs4 import BeautifulSoup
 
 currencies = {
-  "Alteration": 1,
-  "Fusing": 2,
-  "Alchemy": 3,
-  "Chaos": 4,
-  "Gemcutters Prism": 5,
-  "Exalted": 6,
-  "Chromatic": 7,
-  "Jewellers": 8,
-  "Cartographer's Chisel": 10,
-  "Scouring": 11,
-  "Regret": 13,
-  "Regal": 14,
-  "Transmutation": 22,
-  "Augmentation": 23
+  "Alteration": {
+    "id": 1,
+    "tier": 3
+  },
+  "Fusing": {
+    "id": 2,
+    "tier": 2
+  },
+  "Alchemy": {
+    "id": 3,
+    "tier": 3
+  },
+  "Chaos": {
+    "id": 4,
+    "tier": 1
+  },
+  "Gemcutter's Prism": {
+    "id": 5,
+    "tier": 3
+  },
+  "Exalted": {
+    "id": 6,
+    "tier": 0
+  },
+  "Chromatic": {
+    "id": 7,
+    "tier": 3
+  },
+  "Jewellers": {
+    "id": 8,
+    "tier": 3
+  },
+  "Cartographer's Chisel": {
+    "id": 10,
+    "tier": 2
+  },
+  "Scouring": {
+    "id": 11,
+    "tier": 1
+  },
+  "Regret": {
+    "id": 13,
+    "tier": 1
+  },
+  "Regal": {
+    "id": 14,
+    "tier": 1
+  },
+  "Transmutation": {
+    "id": 22,
+    "tier": 4
+  },
+  "Augmentation": {
+    "id": 23,
+    "tier": 4
+  }
 }
 
 def fetch_conversion_offers(league, want, have):
   url = "http://currency.poe.trade/search"
   params = {
     "league": league,
-    "want": currencies[want],
-    "have": currencies[have],
+    "want": currencies[want]["id"],
+    "have": currencies[have]["id"],
     "online": True
   }
 
   r = requests.get(url, params=params)
   offers = parse_conversion_offers(r.text)
+  viable_offers = filter_viable_offers(want, have, offers)
 
   return {
-    "offers": offers,
+    "offers": viable_offers,
     "want": want,
     "have": have,
     "league": league
   }
 
+
+def filter_viable_offers(want, have, offers):
+  return [x for x in offers if is_offer_viable(want, have, x) == True]
+
+
+def is_offer_viable(want, have, offer):
+  # lower tier value means higher value
+  have_tier = currencies[have]["tier"]
+  want_tier = currencies[want]["tier"]
+  conversion_rate = offer["conversion_rate"]
+
+  # if `want` is worth more than `have`, the conversion rate cannot be bigger than 1
+  if have_tier > want_tier and conversion_rate > 1:
+    return False
+  # if `have` is worth more than `have`, the conversion rate cannot be smaller than 1
+  if have_tier < want_tier and conversion_rate < 1:
+    return False
+  return True
+
+
 def parse_conversion_offers(html):
   soup = BeautifulSoup(html, "html.parser")
   rows = soup.find_all(class_="displayoffer")
   parsed_rows = [parse_conversion_offer(x) for x in rows]
-  return [x for x in parsed_rows if x != None]
+  return [x for x in parsed_rows if x != None][:5]
+
 
 def parse_conversion_offer(offer_html):
 
