3aa6c6803b4204f0c32acb6cb574919e9ef209d4
Maximilian Stock <me@maximumstock.net>
2018-05-17 22:35:12 +0200

    Add converter script to build single pickled list from all single pathfinder objects

---
+++ data_analysis/converter.py
@@ -0,0 +1,25 @@
+import os
+import operator
+import pickle
+from datetime import datetime
+
+
+def parse_timestamp(filename):
+  return datetime.strptime(filename.split(".")[0], "%Y_%m_%d_%H_%M_%S").isoformat()
+
+def map_filename(filename):
+  with open("data_analysis/raw/{}".format(filename), "rb") as f:
+    pf = pickle.load(f)
+    return {
+      "created_at": parse_timestamp(filename),
+      "graph": pf.graph,
+      "offers": pf.offers,
+      "results": pf.results,
+      "currencies": pf.currencies
+    }
+
+
+data = [map_filename(x) for x in os.listdir("data_analysis/raw") if "pickle" in x]
+sorted_data = sorted(data, key=operator.itemgetter("created_at"))
+with open("data_analysis/conversion.pickle", "wb") as f:
+  pickle.dump(sorted_data, f)
