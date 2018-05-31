42c7a37288d41ed56b481b2202e4364284e59b9a
Maximilian Stock <me@maximumstock.net>
2018-05-31 21:24:13 +0200

    Add runscript for collecting data

---
+++ run_collector.sh
@@ -0,0 +1,4 @@
+PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Hardcore Incursion" --path "data_analysis/raw/hc_incursion"
+PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Incursion" --path "data_analysis/raw/incursion"
+PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Standard" --path "data_analysis/raw/standard"
+PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Hardcore" --path "data_analysis/raw/hardcore"
