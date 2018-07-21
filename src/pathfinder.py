7f5308c63bed6a0e444f61e3dce7da5315974e3d
Maximilian Stock <me@maximumstock.net>
2018-07-21 17:49:32 +0200

    Add time measuring code to PathFinder implementation to measure needed time for different phases

--- src/pathfinder.py
+++ src/pathfinder.py
@@ -2,11 +2,11 @@ from src.graph import *
 from src.backends import poeofficial
 from src.constants import currencies
 import itertools
-
+import time
 
 
 def test():
-  trading_currencies = list(currencies.keys())[2:4]
+  trading_currencies = list(currencies.keys())
   pf = PathFinder("Incursion", trading_currencies, poeofficial)
   pf.run(3)
 
@@ -18,7 +18,7 @@ def format_conversions(conversions):
 
 
 def format_conversion(conversion):
-  msg = "{} -> {} -- {} ({}) ".format(conversion['from'], conversion['to'], conversion['winnings'], len(conversion['transactions']))
+  msg = "{} -> {} -- {} ({} transactions) ".format(conversion['from'], conversion['to'], conversion['winnings'], len(conversion['transactions']))
   return msg
 
 
@@ -38,10 +38,14 @@ class PathFinder:
 
   def run(self, max_transaction_length=3):
     currency_combinations = list(itertools.permutations(self.currencies, 2))
-    print("Fetching offers for", self.currencies)
+    print("Fetching offers for {} currencies - {} pairs".format(len(self.currencies), len(currency_combinations)))
+    t0 = time.time()
     self.offers = self.backend.fetch_offers(self.league, currency_combinations)
+    t1 = time.time()
+    print("Spent {}s fetching offers".format(round(t1 - t0, 1)))
     self.graph = build_graph(self.offers)
-    print(self.offers)
+    t2 = time.time()
+    print("Spent {}s building the graph".format(round(t2 - t1, 1)))
 
     for c in self.currencies:
       # For currency @c, find all paths within the constructed path that are
@@ -52,14 +56,12 @@ class PathFinder:
         conversion = build_conversion(p)
         if conversion is not None and conversion['winnings'] > 0:
           profitable_conversions.append(conversion)
-      print("Checking {} - {}".format(c, len(profitable_conversions)))
+      print("Checking {} -> {} Conversions".format(c, len(profitable_conversions)))
       profitable_conversions = sorted(profitable_conversions, key=lambda k: k['winnings'], reverse=True)
       self.results[c] = profitable_conversions
-      if len(profitable_conversions) > 0:
-        print(profitable_conversions[0])
-      print(format_conversions(profitable_conversions))
-
-
+      # print(format_conversions(profitable_conversions))
+    t3 = time.time()
+    print("Spent {}s finding paths".format(round(t3 - t2, 1)))
 
 
 if __name__ == '__main__':
