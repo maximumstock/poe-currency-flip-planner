3c1c2896c29b34d4e24e598a2f870035ce096227
Maximilian Stock <me@maximumstock.net>
2018-09-02 01:28:46 +0200

    Fix rate limiting between league collection runs

--- run_collector.sh
+++ run_collector.sh
@@ -1,4 +1,7 @@
 PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Hardcore Delve" --path "data_analysis/raw/hc_delve"
+sleep 10s
 PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Delve" --path "data_analysis/raw/delve"
+sleep 10s
 PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Standard" --path "data_analysis/raw/standard"
+sleep 10s
 PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Hardcore" --path "data_analysis/raw/hardcore"
