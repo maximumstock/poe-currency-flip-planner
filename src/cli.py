52da9a6d05664d1dacda01e7b4ae64d971b209d5
Maximilian Stock <me@maximumstock.net>
2018-09-05 14:26:15 +0200

    Fix CLI options, defaults and documentation. Fix CLI error handling

--- src/cli.py
+++ src/cli.py
@@ -5,8 +5,8 @@ from src.constants import currencies
 from src.backends import poeofficial
 
 
-def log_conversions(conversions):
-  for c in conversions:
+def log_conversions(conversions, currency, limit):
+  for c in conversions[currency][:limit]:
     log_conversion(c)
 
 def log_conversion(c):
@@ -16,12 +16,19 @@ def log_conversion(c):
   print("\n")
 
 parser = argparse.ArgumentParser(description="CLI interface for PathFinder")
-parser.add_argument("--league", default="Incursion", help="League specifier, ie. 'Incursion', 'Hardcore Incursion' or 'Flashback Event (BRE001)'")
-parser.add_argument("--currency", default="all", help="Currency to flip")
-parser.add_argument("--limit", default=3, help="Limit the number of displayed conversions")
+parser.add_argument("--league", default="Delve", help="League specifier, ie. 'Delve', 'Hardcore Delve' or 'Flashback Event (BRE001)'. Defaults to 'Delve'.")
+parser.add_argument("--currency", default="all", help="Full name of currency to flip, ie. 'Cartographer\'s Chisel, or 'Chaos Orb'. See a full list of currency names under src/constants.py. Defaults to all currencies.")
+parser.add_argument("--limit", default=3, help="Limit the number of displayed conversions. Defaults to 3.")
 arguments = parser.parse_args()
 
-chosen_currencies = dict(itertools.islice(currencies.items(), 0, 10))
-p = PathFinder(arguments.league, chosen_currencies, poeofficial)
+league = arguments.league
+currency = arguments.currency
+limit = arguments.limit
+
+chosen_currencies = dict(itertools.islice(currencies.items(), 0, 13))
+p = PathFinder(league, chosen_currencies, poeofficial)
 p.run(3, False)
-log_conversions(p.results[arguments.currency][:arguments.limit])
+try:
+  log_conversions(p.results, currency, limit)
+except KeyError:
+  print("Could not find any proftiable conversions for {} in {}".format(currency, league))
