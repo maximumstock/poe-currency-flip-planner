94b2000958deb25bdf63b00ea69d7645b4ea4ee6
Maximilian Stock <me@maximumstock.net>
2019-01-24 21:20:17 +0100

    Fix data collector cli parsing

--- data_analysis/collector.py
+++ data_analysis/collector.py
@@ -18,14 +18,13 @@ def run():
 
     parser = argparse.ArgumentParser(
         description="data collection tool for PathFinder class")
-    parser.add_argument("--league", default="Delve",
-                        help="League specifier, ie. 'Bestiary', 'Hardcore Bestiary' or 'Flashback Event (BRE001)'")
+    parser.add_argument("--league", default="Betrayal", choices=["Betrayal", "Hardcore Betrayal", "Standard", "Hardcore"],
+                        help="League specifier, ie. 'Betrayal', 'Hardcore Betrayal' or 'Flashback Event (BRE001)'")
     parser.add_argument("--path", default="data_analysis/raw",
                         help="Location where to save collected data")
     arguments = parser.parse_args()
 
-    currencies2 = dict(itertools.islice(currencies.items(), 5))
-    p = PathFinder(arguments.league, currencies2, backend)
+    p = PathFinder(arguments.league, currencies, backend)
     p.run(3)
 
     filename = "{}/{}".format(arguments.path, gen_filename())
