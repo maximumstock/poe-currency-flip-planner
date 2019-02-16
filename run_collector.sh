4c230a8003ee4099e857396ba52a1d99d43de888
Maximilian Stock <me@maximumstock.net>
2019-02-16 12:14:52 +0100

    Feature(): New collector run script to collect unfiltered bulk data from poetrade towards the end of the league, where most interesting bulk items should be available

--- run_collector.sh
+++ run_collector.sh
@@ -1,7 +1,3 @@
-PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Hardcore Betrayal" --path "data_analysis/raw/hc_betrayal" --poetrade
+PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Hardcore Betrayal" --path "data_analysis/raw/hc_betrayal_poetrade" --poetrade --nofilter --fullbulk
 sleep 10s
-PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Betrayal" --path "data_analysis/raw/betrayal" --poetrade
-sleep 10s
-PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Standard" --path "data_analysis/raw/standard" --poetrade
-sleep 10s
-PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Hardcore" --path "data_analysis/raw/hardcore" --poetrade
+PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Betrayal" --path "data_analysis/raw/betrayal_poetrade" --poetrade --nofilter --fullbulk
