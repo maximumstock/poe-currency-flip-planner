56201b13b18cac6f4dd9fde6abacab31ba0c0341
Maximilian Stock <me@maximumstock.net>
2019-06-05 17:19:58 +0200

    feat(): Update collector script for Legion

--- run_collector.sh
+++ run_collector.sh
@@ -1,3 +1,3 @@
-PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Hardcore Synthesis" --path "data_analysis/raw/hc_synthesis_poetrade" --nofilter --fullbulk
+PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Hardcore Legion" --path "data_analysis/raw/hc_legion_poetrade" --nofilter --fullbulk
 sleep 10s
-PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Synthesis" --path "data_analysis/raw/synthesis_poetrade" --nofilter --fullbulk
+PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Legion" --path "data_analysis/raw/legion_poetrade" --nofilter --fullbulk
