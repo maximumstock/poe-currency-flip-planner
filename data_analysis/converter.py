0678f15b8ef8338ca09c05381fd40549b298c9e5
Maximilian Stock <me@maximumstock.net>
2019-10-18 18:41:09 +0200

    refactor: data_analysis converter UX

--- data_analysis/converter.py
+++ data_analysis/converter.py
@@ -5,22 +5,22 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Data Converter")
 parser.add_argument(
-    "--path", help="Folder name of the dumps"
-)
+    "--path", nargs="+", help="Folder name of the dumps", required=True)
 arguments = parser.parse_args()
 
-folder = arguments.path
 
-
-def map_filename(filename):
+def map_filename(folder, filename):
     with open("{}/{}".format(folder, filename), "rb") as f:
         pf = pickle.load(f)
         return pf
 
 
-data = [
-    map_filename(x) for x in os.listdir(folder) if "pickle" in x and "merge" not in x
-]
-sorted_data = sorted(data, key=operator.itemgetter("timestamp"))
-with open("{}/merge.pickle".format(folder), "wb") as f:
-    pickle.dump(sorted_data, f)
+for folder in arguments.path:
+    print("Merging files for {}".format(folder))
+    data = [
+        map_filename(folder, x) for x in os.listdir(folder)
+        if "pickle" in x and "merge" not in x
+    ]
+    sorted_data = sorted(data, key=operator.itemgetter("timestamp"))
+    with open("{}/merge.pickle".format(folder), "wb") as f:
+        pickle.dump(sorted_data, f)
