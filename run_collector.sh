0b916ebcd5ff9f58e5d769b827f45583ac5255d5
Maximilian Stock <me@maximumstock.net>
2020-01-12 17:25:45 +0100

    feat: Update run_collector.sh for Metamorph league

--- run_collector.sh
+++ run_collector.sh
@@ -1,3 +1,3 @@
-PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Hardcore Blight" --path "data_analysis/raw/Blight/Hardcore" --nofilter --fullbulk
+PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Hardcore Metamorph" --path "data_analysis/raw/Metamorph/Hardcore" --nofilter --fullbulk
 sleep 10s
-PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Blight" --path "data_analysis/raw/Blight/Softcore" --nofilter --fullbulk
+PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Metamorph" --path "data_analysis/raw/Metamorph/Softcore" --nofilter --fullbulk
