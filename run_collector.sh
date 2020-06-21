f031b4b40fc40e2d6a4e4c90766d56e05953f660
Maximilian Stock <maximilian.stock@mayflower.de>
2020-06-21 13:42:20 +0200

    feat: update for Harvest league

--- run_collector.sh
+++ run_collector.sh
@@ -1,3 +1,3 @@
-PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Hardcore Delirium" --path "data_analysis/raw/Delirium/Hardcore" --nofilter --fullbulk
+PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Hardcore Harvest" --path "data_analysis/raw/Harvest/Hardcore" --nofilter
 sleep 10s
-PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Delirium" --path "data_analysis/raw/Delirium/Softcore" --nofilter --fullbulk
+PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Harvest" --path "data_analysis/raw/Harvest/Softcore" --nofilter
