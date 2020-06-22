25b3aa39e410ab2667e9361898d32b39f7bcc6e2
Maximilian Stock <maximilian.stock@mayflower.de>
2020-06-22 21:12:05 +0200

    fix: run_collector.sh

--- run_collector.sh
+++ run_collector.sh
@@ -1,3 +1,4 @@
-PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Hardcore Harvest" --path "data_analysis/raw/Harvest/Hardcore" --nofilter
+export PYTHONPATH=$(pwd)
+pipenv run python data_analysis/collector.py --league "Hardcore Harvest" --path "data_analysis/raw/Harvest/Hardcore" --nofilter
 sleep 10s
-PYTHONPATH=$(pwd) python3 data_analysis/collector.py --league "Harvest" --path "data_analysis/raw/Harvest/Softcore" --nofilter
+pipenv run python data_analysis/collector.py --league "Harvest" --path "data_analysis/raw/Harvest/Softcore" --nofilter
