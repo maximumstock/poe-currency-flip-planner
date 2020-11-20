f5721187fe61ec3447cc4863459603e1db41d633
Maximilian Stock <mxmlnstock@googlemail.com>
2020-11-20 20:02:51 +0100

    refactor: finding unique conversions by trader names

--- src/commons.py
+++ src/commons.py
@@ -1,5 +1,6 @@
 import logging
-from typing import List, Dict
+from typing import Any, Dict, List, Set
+
 import numpy as np
 
 league_names = ["Heist", "Hardcore Heist", "Standard", "Hardcore"]
@@ -29,3 +30,23 @@ def load_excluded_traders():
     with open(default_path, "r") as f:
         excluded_traders = [x.strip() for x in f.readlines()]
         return excluded_traders
+
+
+def unique_conversions_by_trader_name(
+        conversions: List[Dict[str, Any]]) -> List[Dict]:
+    seen_traders: Set[str] = set()
+    unique_conversions = []
+
+    for conversion in conversions:
+        trader_names = [t.contact_ign for t in conversion["transactions"]]
+        has_seen_trader = any(
+            [True for x in trader_names if x in seen_traders])
+        if has_seen_trader:
+            continue
+
+        for t in trader_names:
+            seen_traders.add(t)
+
+        unique_conversions.append(conversion)
+
+    return unique_conversions
